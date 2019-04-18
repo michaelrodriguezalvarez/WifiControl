bb9eebaaf0acb797c909cad27693258141818c9a
Michael Rodriguez Alvarez <michaelrodriguezalvarez@gmail.com>
2019-04-18 11:23:08 +0100

    Fix #1 Inicializando el proyecto WifiControl. Fix #2 Creando Pantalla Básica Principal WifiControl. Fix #3 Creando punto de acceso con netsh

---
+++ WifiControl/WifiControl.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WifiControl
+{
+    partial class WifiControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WifiControl));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIdentificador = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxClave = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.labelEstadoBandera = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timerWifiControl = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificador";
+            // 
+            // textBoxIdentificador
+            // 
+            this.textBoxIdentificador.Location = new System.Drawing.Point(109, 20);
+            this.textBoxIdentificador.Name = "textBoxIdentificador";
+            this.textBoxIdentificador.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIdentificador.TabIndex = 1;
+            this.textBoxIdentificador.Text = "WifiControl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Clave";
+            // 
+            // textBoxClave
+            // 
+            this.textBoxClave.Location = new System.Drawing.Point(109, 50);
+            this.textBoxClave.Name = "textBoxClave";
+            this.textBoxClave.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClave.TabIndex = 1;
+            this.textBoxClave.Text = "12345678";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Activar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelEstado
+            // 
+            this.labelEstado.AutoSize = true;
+            this.labelEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEstado.Location = new System.Drawing.Point(13, 136);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(50, 13);
+            this.labelEstado.TabIndex = 0;
+            this.labelEstado.Text = "Estado:";
+            // 
+            // labelEstadoBandera
+            // 
+            this.labelEstadoBandera.AutoSize = true;
+            this.labelEstadoBandera.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEstadoBandera.Location = new System.Drawing.Point(69, 136);
+            this.labelEstadoBandera.Name = "labelEstadoBandera";
+            this.labelEstadoBandera.Size = new System.Drawing.Size(53, 13);
+            this.labelEstadoBandera.TabIndex = 0;
+            this.labelEstadoBandera.Text = "Inactivo";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 132);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(239, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // timerWifiControl
+            // 
+            this.timerWifiControl.Interval = 5000;
+            this.timerWifiControl.Tick += new System.EventHandler(this.timerWifiControl_Tick);
+            // 
+            // WifiControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(239, 154);
+            this.Controls.Add(this.labelEstadoBandera);
+            this.Controls.Add(this.labelEstado);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxClave);
+            this.Controls.Add(this.textBoxIdentificador);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "WifiControl";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wifi Control";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WifiControl_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIdentificador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxClave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelEstado;
+        private System.Windows.Forms.Label labelEstadoBandera;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Timer timerWifiControl;
+    }
+}
+
---
+++ WifiControl/WifiControl.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WifiControl
+{
+    public partial class WifiControl : Form
+    {
+        private string identificador;
+        private string clave;
+        private bool estado;        
+        public WifiControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            this.ActualizarEstadosYComponentes();
+            this.CargarConfiguracionIdentificador();
+        }
+
+        private void CargarConfiguracionIdentificador()
+        {
+            try
+            {
+                StreamReader fichero = new StreamReader("Wificontrol.conf");
+                textBoxIdentificador.Text = fichero.ReadLine();
+                fichero.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }            
+        }
+
+        private void SalvarConfiguracionIdentificador()
+        {
+            try
+            {
+                StreamWriter fichero = new StreamWriter("Wificontrol.conf");
+                fichero.WriteLine(this.identificador);
+                fichero.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void ActualizarEstadosYComponentes()
+        {
+            this.ActualizarEstadoActualServicioWifi();
+            if (this.estado)
+            {
+                labelEstadoBandera.ForeColor = Color.Green;
+                labelEstadoBandera.Text = "Activo";
+                button1.Text = "Desactivar";
+                textBoxIdentificador.Enabled = false;
+                textBoxClave.Enabled = false;
+            }
+            else
+            {
+                labelEstadoBandera.ForeColor = Color.Red;
+                labelEstadoBandera.Text = "Inactivo";
+                button1.Text = "Activar";
+                textBoxIdentificador.Enabled = true;
+                textBoxClave.Enabled = true;
+            }
+        }
+
+        private void ActualizarEstadoActualServicioWifi()
+        {
+            try
+            {
+                string[] salidaArreglo = this.EjecutarProceso("netsh", "wlan show interfaces").Split(':');
+                string salida = salidaArreglo[salidaArreglo.Length - 1];
+                this.estado = (salida.IndexOf("No") == -1) ? true : false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }            
+        }
+
+        private string EjecutarProceso(string pproceso, string pparametros)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(pproceso, pparametros);            
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            Process proceso = Process.Start(startInfo);
+            proceso.WaitForExit();
+            string error = proceso.StandardError.ReadToEnd();
+            if (error != null && error != "")
+            {
+                throw new Exception("Ha ocurido un error al ejecutar el proceso '" + pproceso + "'\n" + "Detalles:\n" + "Error: " + error);
+            }
+            else
+            {
+                return proceso.StandardOutput.ReadToEnd();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.estado)
+                {
+                    this.EjecutarProceso("netsh", "wlan stop hostednetwork");
+                    this.ActualizarEstadosYComponentes();                               
+                }else
+                {
+                    this.identificador = textBoxIdentificador.Text;
+                    this.clave = textBoxClave.Text;
+
+                    if (this.identificador == "")
+                    {
+                        throw new Exception("Debe especificar un identificador para la red.");
+                    }
+
+                    if (this.clave.Length < 8)
+                    {
+                        throw new Exception("Debe especificar una clave con 8 o más caracteres.");
+                    }
+
+                    this.EjecutarProceso("netsh", "wlan set hostednetwork mode=allow ssid="+this.identificador+" key="+this.clave);
+                    this.EjecutarProceso("netsh", "wlan start hostednetwork");
+                    this.ActualizarEstadosYComponentes();
+                    this.SalvarConfiguracionIdentificador();                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timerWifiControl_Tick(object sender, EventArgs e)
+        {
+            this.ActualizarEstadosYComponentes();
+        }
+
+        private void WifiControl_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timerWifiControl.Stop();
+        }
+    }
+}
